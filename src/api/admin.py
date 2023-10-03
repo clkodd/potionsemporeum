ddc1b24cbeafdba393c584cae93830586e0dd101
clkodd <clkodden@calpoly.edu>
2023-10-02 21:10:19 -0700

    removed import

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 from src.api import auth
 import sqlalchemy
 from src import database as db
-import carts
 
 
 router = APIRouter(
@@ -24,8 +23,6 @@ def reset():
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = 100"))
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = 0"))
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = 0"))
-
-    carts.Carts = {}
     
     return "OK"
 
