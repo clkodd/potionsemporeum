55951d5c376b2d640f191714ec1a61034573563d
clkodd <clkodden@calpoly.edu>
2023-10-16 01:17:25 -0700

    sum

--- src/api/audit.py
+++ src/api/audit.py
@@ -20,13 +20,13 @@ def get_inventory():
     with db.engine.begin() as connection:
 
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
-        potions = connection.execute(sqlalchemy.text("SELECT quantity FROM potion_mixes"))
+        potions = connection.execute(sqlalchemy.text("SELECT SUM(quantity) FROM potion_mixes"))
         row1 = result.first()
         
-        total_potions = sum(potions)
+        #total_potions = sum(potions)
         mls = row1.red_ml + row1.blue_ml + row1.green_ml + row1.dark_ml
         
-        return {"number_of_potions": total_potions, "ml_in_barrels": mls, "gold": row1.gold}
+        return {"number_of_potions": potions, "ml_in_barrels": mls, "gold": row1.gold}
 
 
 class Result(BaseModel):
