2ec654db1838558969fdbd91414afa03847e95ca
clkodd <clkodden@calpoly.edu>
2023-11-04 00:07:29 -0700

    big barrels ONLY!

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -135,7 +135,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     running_gold = gold.balance // 3
 
-    if "LARGE" in wholesale_catalog[0].sku and running_gold > 1500:
+    if running_gold > 1500:
         size = "LARGE"
     elif "MEDIUM" in wholesale_catalog[0].sku and running_gold > 800:
         size = "MEDIUM"
